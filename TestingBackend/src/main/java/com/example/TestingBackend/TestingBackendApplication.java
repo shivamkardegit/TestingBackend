0145edac342b3bdf7db24ed10a8f878ae046b077
Shivam <shivam@example.com>
2024-10-10 15:09:01 +0530

    maintotest commin

--- TestingBackend/src/main/java/com/example/TestingBackend/TestingBackendApplication.java
+++ TestingBackend/src/main/java/com/example/TestingBackend/TestingBackendApplication.java
@@ -2,8 +2,17 @@ package com.example.TestingBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com"})
+@EntityScan(basePackages = {"com.beanfiles"})
+@EnableJpaRepositories("com.repositories")
+@EnableAspectJAutoProxy
 public class TestingBackendApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +20,7 @@ public class TestingBackendApplication {
 	}
 
 }
+
+
+
+
